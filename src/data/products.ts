1774a9f4e53e9f0516e1bed899fc1ff74fc436c4
Nguyen Hien <nguyennhienn1701@gmail.com>
2025-08-23 18:48:09 +0700

    refactor: data fake product

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,166 +1,4 @@
-// import type { Product } from '../@types/products.type'
-
-// export const SAMPLE_PRODUCTS: Product[] = [
-//   {
-//     id: 1,
-//     title: 'OnePlus Nord N30 5G | Unlocked Dual-SIM Android Smart Phone | 6.7" LCD Display',
-//     sku: 'HY5480',
-//     stock: 25,
-//     price: 253,
-//     category: 'Electronics',
-//     type: 'Goods',
-//     tags: ['Top rated', 'Best', 'Popular', 'Phone'],
-//     rating: 4.2,
-//     date: '03/12/2023'
-//   },
-//   {
-//     id: 2,
-//     title: 'Socket Mobile Charging Dock',
-//     sku: 'E5480',
-//     stock: 25,
-//     price: 50.5,
-//     category: 'Electronics',
-//     type: 'Goods',
-//     tags: ['Top rated', 'Popular'],
-//     rating: 4.2,
-//     date: '03/12/2023'
-//   },
-//   {
-//     id: 3,
-//     title: 'Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil',
-//     sku: 'XZ25',
-//     stock: 0,
-//     price: 9.2,
-//     category: 'Beauty',
-//     type: 'Goods',
-//     tags: ['Natural'],
-//     rating: 4.2,
-//     date: '03/12/2023'
-//   },
-//   {
-//     id: 4,
-//     title: 'Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil',
-//     sku: 'XZ25',
-//     stock: 5,
-//     price: 9.2,
-//     category: 'Beauty',
-//     type: 'Goods',
-//     tags: ['Natural'],
-//     rating: 4.2,
-//     date: '03/12/2023'
-//   },
-//   {
-//     id: 5,
-//     title: 'Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil',
-//     sku: 'XZ25',
-//     stock: 0,
-//     price: 9.2,
-//     category: 'Beauty',
-//     type: 'Goods',
-//     tags: ['Natural'],
-//     rating: 4.2,
-//     date: '03/12/2023'
-//   },
-//   {
-//     id: 6,
-//     title: 'Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil',
-//     sku: 'XZ25',
-//     stock: 0,
-//     price: 9.2,
-//     category: 'Beauty',
-//     type: 'Goods',
-//     tags: ['Natural'],
-//     rating: 4.2,
-//     date: '03/12/2023'
-//   },
-//   {
-//     id: 7,
-//     title: 'Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil',
-//     sku: 'XZ25',
-//     stock: 2,
-//     price: 9.2,
-//     category: 'Beauty',
-//     type: 'Goods',
-//     tags: ['Natural'],
-//     rating: 4.2,
-//     date: '03/12/2023'
-//   },
-//   {
-//     id: 8,
-//     title: 'Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil',
-//     sku: 'XZ25',
-//     stock: 0,
-//     price: 9.2,
-//     category: 'Beauty',
-//     type: 'Goods',
-//     tags: ['Natural'],
-//     rating: 4.2,
-//     date: '03/12/2023'
-//   },
-//   {
-//     id: 9,
-//     title: 'Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil',
-//     sku: 'XZ25',
-//     stock: 0,
-//     price: 9.2,
-//     category: 'Beauty',
-//     type: 'Goods',
-//     tags: ['Natural'],
-//     rating: 4.2,
-//     date: '03/12/2023'
-//   },
-//   {
-//     id: 10,
-//     title: 'Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil',
-//     sku: 'XZ25',
-//     stock: 0,
-//     price: 9.2,
-//     category: 'Beauty',
-//     type: 'Goods',
-//     tags: ['Natural'],
-//     rating: 4.2,
-//     date: '03/12/2023'
-//   },
-//   {
-//     id: 11,
-//     title: 'Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil',
-//     sku: 'XZ25',
-//     stock: 0,
-//     price: 9.2,
-//     category: 'Beauty',
-//     type: 'Goods',
-//     tags: ['Natural'],
-//     rating: 4.2,
-//     date: '03/12/2023'
-//   },
-//   {
-//     id: 12,
-//     title: 'Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil',
-//     sku: 'XZ25',
-//     stock: 0,
-//     price: 9.2,
-//     category: 'Beauty',
-//     type: 'Goods',
-//     tags: ['Natural'],
-//     rating: 4.2,
-//     date: '03/12/2023'
-//   },
-//   {
-//     id: 13,
-//     title: 'Mielle Organics Rosemary Mint Scalp & Hair Strengthening Oil',
-//     sku: 'XZ25',
-//     stock: 0,
-//     price: 9.2,
-//     category: 'Beauty',
-//     type: 'Goods',
-//     tags: ['Natural'],
-//     rating: 4.2,
-//     date: '03/12/2023'
-//   }
-// ]
-import type { ProductResponse } from '../@types/product.schema'
-
-export const SAMPLE_PRODUCTS: ProductResponse[] = [
+export const SAMPLE_PRODUCTS = [
   {
     id: 1,
     name: 'Nhẫn vàng 18K đính kim cương',
@@ -173,8 +11,7 @@ export const SAMPLE_PRODUCTS: ProductResponse[] = [
     markup_rate: 15,
     selling_price: 9000000,
     warranty_period: 12,
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiu8IOCNccPGcgT15zbsAhaqUNgEm7dQFMQP8gPx8psKN96x1jPnS7lFIEtwvXDxvFU44&usqp=CAU',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ1I8VIYGCOpgaFGHX65OncngCmNpJIo2nGA&s',
     created_at: '2023-12-03',
     updated_at: '2023-12-05'
   },
@@ -211,5 +48,21 @@ export const SAMPLE_PRODUCTS: ProductResponse[] = [
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiu8IOCNccPGcgT15zbsAhaqUNgEm7dQFMQP8gPx8psKN96x1jPnS7lFIEtwvXDxvFU44&usqp=CAU',
     created_at: '2023-12-05',
     updated_at: '2023-12-07'
+  },
+  {
+    id: 4,
+    name: 'Nhẫn vàng 18K đính kim cương',
+    code: 'NV18K001',
+    category_id: 1,
+    weight: 3.5,
+    gold_price_at_time: 5600000,
+    labor_cost: 500000,
+    stone_cost: 2000000,
+    markup_rate: 15,
+    selling_price: 9000000,
+    warranty_period: 12,
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ1I8VIYGCOpgaFGHX65OncngCmNpJIo2nGA&s',
+    created_at: '2023-12-03',
+    updated_at: '2023-12-05'
   }
 ]
